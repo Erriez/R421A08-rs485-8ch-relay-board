84fdb0fed3aa7cc7b8491ba3d16f1e080f7fdbce
Erriez <Erriez@users.noreply.github.com>
2018-03-11 21:41:31 +0100

    Initial checkin.

---
+++ R421A08.py
@@ -0,0 +1,454 @@
+#!/usr/bin/python3
+
+# MIT License
+#
+# Copyright (c) 2018 Erriez
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+
+##
+# 8 Channel RS485 RTU relay board type R421A08.
+#
+# This is a Python example to control the relay board with a USB - RS485 dongle.
+# Python 2.7 and Python 3.6 are supported.
+#
+# Source: https://github.com/Erriez/R421A08-rs485-8ch-relay-board
+#
+
+import argparse
+import serial
+import sys
+import time
+
+# Fixed number of relays on the R421A08 board
+NUM_RELAYS = 8
+
+# Commands
+CMD_ON = 0x01
+CMD_OFF = 0x02
+CMD_TOGGLE = 0x03
+CMD_LATCH = 0x04
+CMD_MOMENTARY = 0x05
+CMD_DELAY = 0x06
+CMD_READ_STATUS = 0x07
+
+# According to MODBUS specification:
+# Wait at least 3.5 char between frames
+# However, some USB - RS485 dongles requires at least 10ms to switch between TX and RX, so use a
+# save delay between frames
+FRAME_DELAY = 0.010
+
+# R421A08 supports MODBUS control command and read status only
+FUNCTION_CONTROL_COMMAND = 0x06
+FUNCTION_READ_STATUS = 0x03
+
+# Fixed receive frame length
+RX_LEN_CONTROL_COMMAND = 8
+RX_LEN_READ_STATUS = 7
+
+# MODBUS CRC tables
+CRC_HI = [
+    0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41, 0x01, 0xC0, 0x80, 0x41, 0x00, 0xC1, 0x81, 0x40,
+    0x01, 0xC0, 0x80, 0x41, 0x00, 0xC1, 0x81, 0x40, 0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41,
+    0x01, 0xC0, 0x80, 0x41, 0x00, 0xC1, 0x81, 0x40, 0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41,
+    0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41, 0x01, 0xC0, 0x80, 0x41, 0x00, 0xC1, 0x81, 0x40,
+    0x01, 0xC0, 0x80, 0x41, 0x00, 0xC1, 0x81, 0x40, 0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41,
+    0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41, 0x01, 0xC0, 0x80, 0x41, 0x00, 0xC1, 0x81, 0x40,
+    0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41, 0x01, 0xC0, 0x80, 0x41, 0x00, 0xC1, 0x81, 0x40,
+    0x01, 0xC0, 0x80, 0x41, 0x00, 0xC1, 0x81, 0x40, 0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41,
+    0x01, 0xC0, 0x80, 0x41, 0x00, 0xC1, 0x81, 0x40, 0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41,
+    0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41, 0x01, 0xC0, 0x80, 0x41, 0x00, 0xC1, 0x81, 0x40,
+    0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41, 0x01, 0xC0, 0x80, 0x41, 0x00, 0xC1, 0x81, 0x40,
+    0x01, 0xC0, 0x80, 0x41, 0x00, 0xC1, 0x81, 0x40, 0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41,
+    0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41, 0x01, 0xC0, 0x80, 0x41, 0x00, 0xC1, 0x81, 0x40,
+    0x01, 0xC0, 0x80, 0x41, 0x00, 0xC1, 0x81, 0x40, 0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41,
+    0x01, 0xC0, 0x80, 0x41, 0x00, 0xC1, 0x81, 0x40, 0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41,
+    0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41, 0x01, 0xC0, 0x80, 0x41, 0x00, 0xC1, 0x81, 0x40
+]
+
+CRC_LOW = [
+    0x00, 0xC0, 0xC1, 0x01, 0xC3, 0x03, 0x02, 0xC2, 0xC6, 0x06, 0x07, 0xC7, 0x05, 0xC5, 0xC4, 0x04,
+    0xCC, 0x0C, 0x0D, 0xCD, 0x0F, 0xCF, 0xCE, 0x0E, 0x0A, 0xCA, 0xCB, 0x0B, 0xC9, 0x09, 0x08, 0xC8,
+    0xD8, 0x18, 0x19, 0xD9, 0x1B, 0xDB, 0xDA, 0x1A, 0x1E, 0xDE, 0xDF, 0x1F, 0xDD, 0x1D, 0x1C, 0xDC,
+    0x14, 0xD4, 0xD5, 0x15, 0xD7, 0x17, 0x16, 0xD6, 0xD2, 0x12, 0x13, 0xD3, 0x11, 0xD1, 0xD0, 0x10,
+    0xF0, 0x30, 0x31, 0xF1, 0x33, 0xF3, 0xF2, 0x32, 0x36, 0xF6, 0xF7, 0x37, 0xF5, 0x35, 0x34, 0xF4,
+    0x3C, 0xFC, 0xFD, 0x3D, 0xFF, 0x3F, 0x3E, 0xFE, 0xFA, 0x3A, 0x3B, 0xFB, 0x39, 0xF9, 0xF8, 0x38,
+    0x28, 0xE8, 0xE9, 0x29, 0xEB, 0x2B, 0x2A, 0xEA, 0xEE, 0x2E, 0x2F, 0xEF, 0x2D, 0xED, 0xEC, 0x2C,
+    0xE4, 0x24, 0x25, 0xE5, 0x27, 0xE7, 0xE6, 0x26, 0x22, 0xE2, 0xE3, 0x23, 0xE1, 0x21, 0x20, 0xE0,
+    0xA0, 0x60, 0x61, 0xA1, 0x63, 0xA3, 0xA2, 0x62, 0x66, 0xA6, 0xA7, 0x67, 0xA5, 0x65, 0x64, 0xA4,
+    0x6C, 0xAC, 0xAD, 0x6D, 0xAF, 0x6F, 0x6E, 0xAE, 0xAA, 0x6A, 0x6B, 0xAB, 0x69, 0xA9, 0xA8, 0x68,
+    0x78, 0xB8, 0xB9, 0x79, 0xBB, 0x7B, 0x7A, 0xBA, 0xBE, 0x7E, 0x7F, 0xBF, 0x7D, 0xBD, 0xBC, 0x7C,
+    0xB4, 0x74, 0x75, 0xB5, 0x77, 0xB7, 0xB6, 0x76, 0x72, 0xB2, 0xB3, 0x73, 0xB1, 0x71, 0x70, 0xB0,
+    0x50, 0x90, 0x91, 0x51, 0x93, 0x53, 0x52, 0x92, 0x96, 0x56, 0x57, 0x97, 0x55, 0x95, 0x94, 0x54,
+    0x9C, 0x5C, 0x5D, 0x9D, 0x5F, 0x9F, 0x9E, 0x5E, 0x5A, 0x9A, 0x9B, 0x5B, 0x99, 0x59, 0x58, 0x98,
+    0x88, 0x48, 0x49, 0x89, 0x4B, 0x8B, 0x8A, 0x4A, 0x4E, 0x8E, 0x8F, 0x4F, 0x8D, 0x4D, 0x4C, 0x8C,
+    0x44, 0x84, 0x85, 0x45, 0x87, 0x47, 0x46, 0x86, 0x82, 0x42, 0x43, 0x83, 0x41, 0x81, 0x80, 0x40
+]
+
+
+def modbus_crc(data):
+    """
+        brief Calculate MODBUS crc
+    :param data: Data
+    :return: List CRC high Byte, CRC low Byte
+    """
+    crc_high = 0xFF
+    crc_low = 0xFF
+
+    for i in range(0, len(data)):
+        index = crc_high ^ data[i]
+        crc_high = crc_low ^ CRC_HI[index]
+        crc_low = CRC_LOW[index]
+
+    return [crc_high, crc_low]
+
+
+def print_frame(msg, data):
+    """
+        Print frame
+    :param msg: Message before the data
+    :param data: List data (int)
+    :return: None
+    """
+    line = '%s %d: ' % (msg, len(data))
+    for i in range(0, len(data)):
+        if line:
+            line += ' '
+        line += '%02X' % data[i]
+    print(line)
+
+
+def modbus_send(ser, tx_data, verbose=False):
+    """
+        MODBUS send
+    :param ser: Serial
+    :param tx_data: List data (int)
+    :param verbose: Print TX frame
+    :return: None
+    """
+    # Print transmit frame
+    if verbose:
+        print_frame('TX', tx_data)
+
+    # Write binary command to relay card over serial port
+    try:
+        ser.write(tx_data)
+    except serial.SerialTimeoutException:
+        print('TX error: Serial write timeout')
+    except serial.SerialException:
+        print('TX error: Serial write failed')
+
+    # Wait between transmitting frames
+    time.sleep(FRAME_DELAY)
+
+
+def modbus_receive(ser, rx_length, verbose=False):
+    """
+        MODBUS receive
+    :param ser: Serial
+    :param rx_length: Receive length
+    :param verbose: Print RX frame
+    :return: List received data (int)
+    """
+    # Read response with timeout
+    rx_data = None
+    try:
+        rx_data = ser.read(rx_length)
+    except serial.SerialException:
+        print('RX error: Serial read failed')
+
+    # Check read timeout
+    if rx_data:
+        # Convert bytearray to list
+        if sys.version_info[0] >= 3:
+            # Python 3 and higher
+            rx_data = [int(i) for i in rx_data]
+        else:
+            # Python 2 and lower
+            rx_data = [ord(i) for i in rx_data]
+
+        # Print receive frame
+        if verbose:
+            print_frame('RX', rx_data)
+
+        # Check response: TX data must be the same as RX data
+        if len(rx_data) != rx_length:
+            print('RX error: Incorrect receive length {} Bytes, expected {} Bytes.'.format(
+                len(rx_data),
+                rx_length))
+    else:
+        print('RX error: Receive timeout')
+
+    return rx_data
+
+
+def modbus_listen(ser, address):
+    """
+        Listen for incoming packages in an endless loop
+    :param ser: Serial
+    :param address:
+        0: Print all addresses
+        1..255: Print address only
+    :return: None
+    """
+    ser.timeout = FRAME_DELAY
+    rx_data = []
+    while 1:
+        c = ser.read(1)
+        if c:
+            rx_data.append(ord(c))
+        if rx_data and not c:
+            if not address or rx_data[0] == address:
+                print_frame('RX', rx_data)
+            rx_data = []
+
+
+def send_relay_command(ser, address, relay, cmd, delay=0, verbose=False):
+    """
+        Send relay control
+    :param ser: Serial
+    :param address: Address
+    :param relay: Relay number
+    :param cmd: Command
+    :param delay: Optional delay
+    :param verbose: Print verbose messages
+    :return: List response (int)
+    """
+    # Create binary control command
+    tx_data = [
+        address,                    # Slave address of the relay board 0..63
+        FUNCTION_CONTROL_COMMAND,   # Read status is always 0x03
+        0x00, relay,                # Relay 0x0001..0x0008
+        cmd,                        # Command 0x01..0x06
+        delay                       # Delay 0x00..0xFF
+    ]
+    # Append CRC to transmit frame
+    tx_data += modbus_crc(tx_data)
+
+    # Send command
+    modbus_send(ser, tx_data, verbose)
+
+    # Wait for response with timeout
+    return modbus_receive(ser, RX_LEN_CONTROL_COMMAND, verbose)
+
+
+def read_relay_status(ser, address, relay, verbose=False):
+    """
+        Read relay status
+    :param ser: Serial
+    :param address: Address
+    :param relay: Relay number
+    :param verbose: Print verbose messages
+    :return:
+        0: Relay off
+        1: Relay on
+        -1: An error occurred
+    """
+    # Create binary read status
+    tx_data = [
+        address,                # Slave address of the relay board 0..63
+        FUNCTION_READ_STATUS,   # Read status is always 0x03
+        0x00, relay,            # Relay 0x0001..0x0008
+        0x00, 0x01              # Number of bytes is always 0x0001
+    ]
+
+    # Append CRC
+    tx_data += modbus_crc(tx_data)
+
+    # Send command and wait for response with timeout
+    modbus_send(ser, tx_data, verbose)
+
+    # Wait for response with timeout
+    rx_data = modbus_receive(ser, RX_LEN_READ_STATUS, verbose)
+
+    if rx_data and len(rx_data) > 2:
+        # Check CRC
+        data_no_crc = rx_data[:-2]
+        crc = rx_data[-2:]
+        if modbus_crc(data_no_crc) != crc:
+            print('RX error: Incorrect CRC received')
+        elif rx_data[0] != tx_data[0]:
+            print('RX error: Incorrect address received')
+        elif rx_data[1] != tx_data[1]:
+            print('RX error: Incorrect function received')
+        elif rx_data[2] != 2:
+            print('RX error: Incorrect data length received')
+        elif rx_data[3] != 0:
+            print('RX error: Incorrect data high Byte received')
+        elif rx_data[4] != 0 and rx_data[4] != 1:
+            print('RX error: Incorrect data low Byte received')
+        else:
+            return rx_data[4]
+
+    return -1
+
+
+def check_relay_numbers(relays):
+    """
+        Check for valid relay numbers. Exit when errors found.
+    :param relays: List relay numbers (int)
+    :return: None
+    """
+    errors = False
+    for relay in relays:
+        if relay < 1 or relay > NUM_RELAYS:
+            errors = True
+            print('Incorrect relay number: {}'.format(relay))
+    if errors:
+        sys.exit(1)
+
+
+def main():
+    """
+        Main function, including argument parser
+    :return: None
+    """
+
+    # Argument parser
+    _parser = argparse.ArgumentParser(description='8 Channel RS485 RTU relay board type R421A08')
+    _parser.add_argument('SERIAL_PORT',
+                         help='Serial port (such as COM1)')
+    _parser.add_argument('ADDRESS',
+                         type=int,
+                         help='Slave address [0..63]')
+
+    _parser.add_argument('-v', '--verbose',
+                         action='store_true',
+                         help='Print verbose')
+    _parser.add_argument('-i', '--listen',
+                         action='store_true',
+                         help='Listen on receive')
+    _parser.add_argument('-r', '--relay',
+                         nargs='*',
+                         help='Relay numbers [1..8] or * for all relays')
+    _parser.add_argument('-s', '--status',
+                         action='store_true',
+                         help='Read status')
+    _parser.add_argument('-1', '--on',
+                         action='store_true',
+                         help='On')
+    _parser.add_argument('-0', '--off',
+                         action='store_true',
+                         help='Off')
+    _parser.add_argument('-t', '--toggle',
+                         action='store_true',
+                         help='Toggle')
+    _parser.add_argument('-l', '--latch',
+                         action='store_true',
+                         help='Latch')
+    _parser.add_argument('-m', '--momentary',
+                         action='store_true',
+                         help='Momentary')
+    _parser.add_argument('-d', '--delay',
+                         type=int,
+                         help='Delay (0..255 seconds)')
+    args = _parser.parse_args()
+
+    # Get arguments
+    serial_port = args.SERIAL_PORT
+    address = args.ADDRESS
+    relays = None
+    if args.relay:
+        if args.relay == ['*']:
+            # Convert * to relay int numbers
+            relays = [relay for relay in range(1, NUM_RELAYS + 1)]
+        else:
+            # Convert relay arguments to int
+            relays = list(map(int, args.relay))
+        check_relay_numbers(relays)
+    delay = 0
+    verbose = args.verbose
+
+    # Create serial
+    ser = serial.Serial()
+    ser.port = serial_port
+    ser.baudrate = 9600
+    ser.bytesize = 8
+    ser.stopbits = 1
+    ser.parity = serial.PARITY_NONE
+    ser.timeout = 0.1
+
+    # Open serial port
+    try:
+        ser.open()
+    except serial.SerialException:
+        print('Error: Cannot open serial port: ' + ser.port)
+        sys.exit(1)
+
+    cmd = None
+    if args.listen:
+        if address == 0:
+            print('Listening for all incoming frames.')
+        else:
+            print('Listening for incoming frames from address {}.'.format(address))
+        print('Press CTRL+C to abort.')
+        modbus_listen(ser, address)
+    else:
+        if args.status:
+            cmd = CMD_READ_STATUS
+        elif args.on:
+            cmd = CMD_ON
+            print('Turn relay(s) {} on...'.format(relays))
+        elif args.off:
+            cmd = CMD_OFF
+            print('Turn relay(s) {} off...'.format(relays))
+        elif args.toggle:
+            cmd = CMD_TOGGLE
+            print('Toggle relay(s) {}...'.format(relays))
+        elif args.latch:
+            cmd = CMD_LATCH
+            print('Latch relay(s) {}...'.format(relays))
+        elif args.momentary:
+            cmd = CMD_MOMENTARY
+            print('Turn relay(s) {} on/off...'.format(relays))
+        elif args.delay:
+            cmd = CMD_DELAY
+            delay = args.delay
+            print('Turn relay(s) {} on/off with delay {} sec...'.format(relays, delay))
+        else:
+            print('Error: No command option specified')
+            sys.exit(0)
+
+    # Send command
+    if cmd == CMD_READ_STATUS:
+        for relay in list(relays):
+            status = read_relay_status(ser,
+                                       address=address,
+                                       relay=relay,
+                                       verbose=verbose)
+            if status == 1:
+                status_str = 'ON'
+            elif status == 0:
+                status_str = 'OFF'
+            else:
+                status_str = 'UNKNOWN'
+            print('Relay {}: {}'.format(relay, status_str))
+    else:
+        for relay in list(relays):
+            send_relay_command(ser,
+                               address=address,
+                               relay=relay,
+                               cmd=cmd,
+                               delay=delay,
+                               verbose=verbose)
+
+
+if __name__ == '__main__':
+    main()
---
+++ README.md
@@ -0,0 +1,220 @@
+# 8 Channel RS485 MODBUS RTU relay board type R421A08
+
+This is a Python example to control the relay board with a USB - RS485 dongle.
+Python 2 and 3 are supported.
+
+## Hardware
+
+This following hardware is required for this project:
+
+### R421A08 relay board:
+
+![R421A08 board](https://raw.githubusercontent.com/Erriez/R421A08-rs485-8ch-relay-board/master/images/R421A08.png)
+
+* Board power: 12V DC 
+* Current board idle: ~11mA
+* Current one relay: ~26mA
+* Current all relays on: ~220mA
+
+### RS485 - USB dongle:
+
+This project requires a USB to RS485 dongle, for example:
+
+![RS485 - USB dongle](https://raw.githubusercontent.com/Erriez/R421A08-rs485-8ch-relay-board/master/images/RS485_USB_dongle.png)
+
+## RS485 RTU MODBUS communication protocol
+
+This is an open binary communication protocol. This relay board does **not** support ASCII mode.
+
+* Serial port at 9600 baud, 8 databits, 1 stop, no parity.
+* The board supports MODBUS RTU protocol only.
+* The board supports 64 addresses, configurable with 6 DIP switches. 
+* The board does not support ACII mode.
+* The board does not broadcasts.
+* All relays are off after power-on.
+
+## Installation PySerial Windows
+```bash
+cd c:\Python36
+python.exe -m pip install pyserial
+```
+
+## Installation PySerial Linux
+```bash
+$ sudo python3 -m pip install pyserial
+```
+
+## Usage
+```bash
+c:\Python36\python.exe R421A08.py -h
+usage: R421A08.py [-h] [-v] [-i] [-r [RELAY [RELAY ...]]] [-s] [-1] [-0] [-t]
+                  [-l] [-m] [-d DELAY]
+                  SERIAL_PORT ADDRESS
+
+8 Channel RS485 RTU relay board type R421A08
+
+positional arguments:
+  SERIAL_PORT           Serial port (such as COM1)
+  ADDRESS               Slave address [0..63]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -v, --verbose         Print verbose
+  -i, --listen          Listen on receive
+  -r [RELAY [RELAY ...]], --relay [RELAY [RELAY ...]]
+                        Relay numbers [1..8] or * for all relays
+  -s, --status          Read status
+  -1, --on              On
+  -0, --off             Off
+  -t, --toggle          Toggle
+  -l, --latch           Latch
+  -m, --moment          Momentary
+  -d DELAY, --delay DELAY
+                        Delay (0..255 seconds)
+```
+
+### Board address 1, turn relay 1 on
+```bash
+python R421A08.py COM3 1 --relay 1 --on
+```
+
+### Board address 1, turn all relays on
+```bash
+python R421A08.py COM3 1 --relay * --on
+```
+
+### Board address 1, turn relay 1 off
+```bash
+python R421A08.py COM3 1 --relay 1 --off
+```
+
+### Board address 1, toggle relay 2
+```bash
+python R421A08.py COM3 1 --relay 2 --toggle
+```
+
+### Board address 1, read status relay 1
+```bash
+python R421A08.py COM3 1 --relay 1 --status
+Relay 1: OFF
+```
+
+### Board address 1, read status all relay's
+```bash
+python R421A08.py COM3 1 --relay * --status
+Relay 1: ON
+Relay 2: OFF
+Relay 3: ON
+Relay 4: ON
+Relay 5: ON
+Relay 6: ON
+Relay 7: ON
+Relay 8: OFF
+```
+
+### Listen all addresses
+```bash
+python  R421A08.py COM3 0 --listen
+Listening for all incoming frames.
+Press CTRL+C to abort.
+RX 16:  02 06 00 02 01 00 29 9A 01 06 00 02 01 00 29 9A
+RX 16:  01 06 00 01 01 00 D9 9A 01 06 00 01 01 00 D9 9A
+RX 16:  01 06 00 01 02 00 D9 6A 01 06 00 01 02 00 D9 6A
+RX 16:  01 06 00 01 03 00 D8 FA 01 06 00 01 03 00 D8 FA
+```
+
+### Listen address 2
+```bash
+python  R421A08.py COM3 2 --listen
+Listening for all incoming frames.
+Press CTRL+C to abort.
+RX 16:  02 06 00 01 01 00 D9 A9 02 06 00 01 01 00 D9 A9
+RX 16:  02 06 00 01 02 00 D9 59 02 06 00 01 02 00 D9 59
+```
+
+### Verbose
+```bash
+python R421A08.py COM3 1 --relay * --on -v
+Turn relay(s) [1, 2, 3, 4, 5, 6, 7, 8] on...
+TX 8:  01 06 00 01 01 00 D9 9A
+RX 8:  01 06 00 01 01 00 D9 9A
+TX 8:  01 06 00 02 01 00 29 9A
+RX 8:  01 06 00 02 01 00 29 9A
+TX 8:  01 06 00 03 01 00 78 5A
+RX 8:  01 06 00 03 01 00 78 5A
+TX 8:  01 06 00 04 01 00 C9 9B
+RX 8:  01 06 00 04 01 00 C9 9B
+TX 8:  01 06 00 05 01 00 98 5B
+RX 8:  01 06 00 05 01 00 98 5B
+TX 8:  01 06 00 06 01 00 68 5B
+RX 8:  01 06 00 06 01 00 68 5B
+TX 8:  01 06 00 07 01 00 39 9B
+RX 8:  01 06 00 07 01 00 39 9B
+TX 8:  01 06 00 08 01 00 09 98
+RX 8:  01 06 00 08 01 00 09 98
+
+python R421A08.py COM3 1 --relay * --off -v
+Turn relay(s) [1, 2, 3, 4, 5, 6, 7, 8] off...
+TX 8:  01 06 00 01 02 00 D9 6A
+RX 8:  01 06 00 01 02 00 D9 6A
+TX 8:  01 06 00 02 02 00 29 6A
+RX 8:  01 06 00 02 02 00 29 6A
+TX 8:  01 06 00 03 02 00 78 AA
+RX 8:  01 06 00 03 02 00 78 AA
+TX 8:  01 06 00 04 02 00 C9 6B
+RX 8:  01 06 00 04 02 00 C9 6B
+TX 8:  01 06 00 05 02 00 98 AB
+RX 8:  01 06 00 05 02 00 98 AB
+TX 8:  01 06 00 06 02 00 68 AB
+RX 8:  01 06 00 06 02 00 68 AB
+TX 8:  01 06 00 07 02 00 39 6B
+RX 8:  01 06 00 07 02 00 39 6B
+TX 8:  01 06 00 08 02 00 09 68
+RX 8:  01 06 00 08 02 00 09 68
+
+python R421A08.py COM3 1 --relay 1 --toggle -v
+Toggle relay(s) [1]...
+TX 8:  01 06 00 01 03 00 D8 FA
+RX 8:  01 06 00 01 03 00 D8 FA
+
+python R421A08.py COM3 1 --relay 1 --latch -v
+Latch relay(s) [1]...
+TX 8:  01 06 00 01 04 00 DA CA
+RX 8:  01 06 00 01 04 00 DA CA
+
+python R421A08.py COM3 1 --relay 1 --momentary -v
+Turn relay(s) [1] on/off...
+TX 8:  01 06 00 01 05 00 DB 5A
+RX 8:  01 06 00 01 05 00 DB 5A
+
+python R421A08.py COM3 1 --relay 1 --delay 3 -v
+Turn relay(s) [1] on/off with delay 3 sec...
+TX 8:  01 06 00 01 06 03 9B AB
+RX 8:  01 06 00 01 06 03 9B AB
+
+python R421A08.py COM3 1 --relay * --status -v
+TX 8:  01 03 00 01 00 01 D5 CA
+RX 7:  01 03 02 00 01 79 84
+Relay 1: ON
+TX 8:  01 03 00 02 00 01 25 CA
+RX 7:  01 03 02 00 01 79 84
+Relay 2: ON
+TX 8:  01 03 00 03 00 01 74 0A
+RX 7:  01 03 02 00 01 79 84
+Relay 3: ON
+TX 8:  01 03 00 04 00 01 C5 CB
+RX 7:  01 03 02 00 01 79 84
+Relay 4: ON
+TX 8:  01 03 00 05 00 01 94 0B
+RX 7:  01 03 02 00 01 79 84
+Relay 5: ON
+TX 8:  01 03 00 06 00 01 64 0B
+RX 7:  01 03 02 00 01 79 84
+Relay 6: ON
+TX 8:  01 03 00 07 00 01 35 CB
+RX 7:  01 03 02 00 01 79 84
+Relay 7: ON
+TX 8:  01 03 00 08 00 01 05 C8
+RX 7:  01 03 02 00 00 B8 44
+Relay 8: OFF
+```
